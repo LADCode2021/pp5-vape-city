799d72a706b57a600dac8027b399f163c6788cf7
LADCode2021 <luke.dawson@live.co.uk>
2022-12-14 08:19:28 +0000

    Initial deploy

--- pp5_vape_city/settings.py
+++ pp5_vape_city/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-4n2=$n^s7g%zegf6y8-9i9qke90q_&lytu5dbo*s)4-+hz_p+v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pp5-vape-city.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -100,15 +101,17 @@ LOGIN_REDIRECT_URL = '/'
 WSGI_APPLICATION = 'pp5_vape_city.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
