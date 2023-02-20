cb642a8601ce8fc5d7ee2e6a6f087e467e0dee92
LADCode2021 <luke.dawson@live.co.uk>
2023-02-20 10:55:04 +0000

    Add a display_home option to Products app models.py

--- products/models.py
+++ products/models.py
@@ -27,6 +27,7 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    display_home = models.BooleanField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
     class Meta:
