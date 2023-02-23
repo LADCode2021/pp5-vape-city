eba42cf10ccfea4d6db4318de2aed0342f548e72
LADCode2021 <luke.dawson@live.co.uk>
2023-02-23 23:23:22 +0000

    Commit to be able to revert

--- basket/views.py
+++ basket/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect
 
+# Create your views here.
+
 def view_basket(request):
-    """ A view that renders the basket contents page """
+    """ A view that renders the bag contents page """
 
-    return render(request, 'basket/basket.html')
-
+    return render(request, 'bag/bag.html')
 
 def add_to_basket(request, item_id):
-    """ Add a quantity of the specified product to the shopping basket """
+    """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -19,4 +20,5 @@ def add_to_basket(request, item_id):
         basket[item_id] = quantity
 
     request.session['basket'] = basket
+    print(request.session['basket'])
     return redirect(redirect_url)
