d8c4bd8dbdc27d058c8e11e6e38b20f279aaa03a
LADCode2021 <luke.dawson@live.co.uk>
2023-02-24 12:56:12 +0000

    Add adjust and remove basket views, add calc subtotal template tags functionality

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -34,3 +34,53 @@ def add_to_basket(request, item_id):
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+
+def adjust_basket(request, item_id):
+    """ Add a quantity of the specified product to the shopping basket """
+
+    quantity = int(request.POST.get('quantity'))
+    flavour = None
+    if 'product_flavour' in request.POST:
+        flavour = request.POST['product_flavour']
+    basket = request.session.get('basket', {})
+
+    if flavour:
+        if quantity > 0:
+            basket[item_id]['items_by_flavour'][flavour] = quantity
+        else:
+            del basket[item_id]['items_by_flavour'][flavour]
+            if not basket[item_id]['items_by_flavour']:
+                basket.pop(item_id)
+
+    else:
+        if quantity > 0:
+            basket[item_id] = quantity
+        else:
+            basket.pop(item_id)
+
+    request.session['basket'] = basket
+    return redirect(reverse(view_basket))
+
+
+def remove_from_basket(request, item_id):
+    """Remove the item from the shopping basket"""
+
+    try:
+        flavour = None
+        if 'product_flavour' in request.POST:
+            flavour = request.POST['product_flavour']
+        basket = request.session.get('basket', {})
+
+        if flavour:
+            del basket[item_id]['items_by_flavour'][flavour]
+            if not basket[item_id]['items_by_flavour']:
+                basket.pop(item_id)
+        else:
+            basket.pop(item_id)
+
+        request.session['basket'] = basket
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
