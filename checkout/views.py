4790a56e082b2c079235fc1917297e8b541ffacb
LADCode2021 <luke.dawson@live.co.uk>
2023-02-25 22:17:23 +0000

    Add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MfVi9GbWcf4VCYGwaATeqkBAP7hYL3222TFfmUKP2IGJLetEBVOHgfaHUB4lRhAupd5DSvCdAB2ALqipNL7sjcd00gh06P96h',
+        'client_secret': 'Test Client Secret',
     }
 
     return render(request, template, context)
